a7d1a8d42e28af6b2e054eaaf0624b4f58c981f4
Anas <admin@fqmsys.com>
2024-09-27 15:01:48 +0500

    localstorage implemented

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const pickButton = document.querySelector("#pick-color")
-const selectedColor = document.querySelector("#selectedColor")
+const pickButton = document.querySelector("#pick-color");
+const selectedColor = document.querySelector("#selectedColor");
+const storedColor = localStorage.getItem("selectedColor") || "";
 
+//Function to pick color 
 const pickColor = async ()=>{
     try{
         document.body.style.display = "none";
         const eyeDropper = new EyeDropper();
         const {sRGBHex} = await eyeDropper.open();
-        navigator.clipboard.writeText(sRGBHex)
+        navigator.clipboard.writeText(sRGBHex);
+        localStorage.setItem("selectedColor", sRGBHex);
         document.body.style.display = "block";
         selectedColor.textContent = sRGBHex;
         selectedColor.style.background = sRGBHex;
     }catch(error){
-        alert(error)
+        alert(error);
     }
 }
-
-pickButton.addEventListener("click", pickColor)
+//Function to load Previously Picked Color
+const loadPreviousColor = async()=>{
+    try{
+        console.log(storedColor)
+        selectedColor.textContent = storedColor;
+        selectedColor.style.background = storedColor;
+    }catch(error){
+        alert(error);
+    }
+}
+//Load Previously Picked Color
+loadPreviousColor();
+//Click Event of PickButton
+pickButton.addEventListener("click", pickColor);
